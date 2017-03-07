44c58094ac815fc7fb1f8716c4d11f61f0bd6bbe
Matthew Slight <matthew@kryptolabs.com>
2017-03-07 14:48:03 +0400

    added swiper

--- app/TestScene.js
+++ app/TestScene.js
@@ -1,29 +1,54 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Text, TouchableHighlight } from 'react-native';
+import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
+import Swiper from 'react-native-swiper'
+import randomcolor from 'randomcolor'
 
 export default class TestScene extends Component {
+
+    viewStyle() {
+        return {
+            flex: 1,
+            backgroundColor: randomcolor(),
+            justifyContent: 'center',
+            alignItems: 'center',
+        }
+    }
+
     render() {
         return (
-            <View style={{flex: 1}}>
-                <View style={{flex: 1, backgroundColor: 'powderblue'}}/>
-                    <Text>Current Scene: {this.props.title}</Text>
-                <View style={{flex: 2, backgroundColor: 'skyblue'}}>
-                    <TouchableHighlight onPress={this.props.onForward}>
-                        <Text>Tap me to load the next scene</Text>
-                    </TouchableHighlight>
-
-                    <TouchableHighlight onPress={this.props.onBack}>
-                        <Text>Tap me to go back</Text>
-                    </TouchableHighlight>
+            <Swiper horizontal={false} showsPagination={false} index={1}>
+                <View style={this.viewStyle()}>
+                    <TitleText label="Up" />
                 </View>
-                <View style={{flex: 3, backgroundColor: 'steelblue'}}/>
-            </View>
+                <Swiper loop={true} showsPagination={false}>
+                    <View style={this.viewStyle()}>
+                        <TitleText label="Channel 1" />
+                    </View>
+                    <View style={this.viewStyle()}>
+                        <TitleText label="Channel 2" />
+                    </View>
+                    <View style={this.viewStyle()}>
+                        <TitleText label="Channel 3" />
+                    </View>
+                    <View style={this.viewStyle()}>
+                        <TitleText label="Channel 4" />
+                    </View>
+                </Swiper>
+                <View style={this.viewStyle()}>
+                    <TitleText label="Down" />
+                </View>
+            </Swiper>
+
         )
     }
 }
 
-TestScene.propTypes = {
-    title: PropTypes.string.isRequired,
-    onForward: PropTypes.func.isRequired,
-    onBack: PropTypes.func.isRequired,
-};
+class TitleText extends React.Component {
+    render() {
+        return (
+            <Text style={{ fontSize: 48, color: 'white' }}>
+                {this.props.label}
+            </Text>
+        )
+    }
+}
